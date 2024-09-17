502f322415e12c9fec8f5a45a2f6f83112e99f3d
megesh132 <megesh2003@gmail.com>
2024-09-17 20:16:50 +0530

    Your commit message

--- script.js
+++ script.js
@@ -37,3 +37,25 @@ function slideCarousel() {
 }
 
 setInterval(slideCarousel, 2000); // Slide every 2 seconds
+// Get the modal
+var modal = document.getElementById("popupModal");
+
+// Get the <span> element that closes the modal
+var closeBtn = document.getElementsByClassName("close-btn")[0];
+
+// Show the modal when the page loads
+window.onload = function() {
+  modal.style.display = "block";
+};
+
+// Close the modal when the close button is clicked
+closeBtn.onclick = function() {
+  modal.style.display = "none";
+};
+
+// Close the modal if the user clicks anywhere outside of the modal content
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
